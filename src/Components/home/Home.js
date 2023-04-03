102e0b8936ea12426f2ae9c999332c6c6dc36e38
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-04-03 02:59:41 +0000

    i am solving some problem

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,13 +1,12 @@
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import Tours from '../tours/Tours';
-const memesData = require("../../data/db.json")
 function Home(params) {
     return (
         <>
             <Header />
             {
-                memesData.map(element => {
+                params.memesData.map(element => {
                     return (
                         <Tours tours_name={element.name} image_url={element.image} />
 
